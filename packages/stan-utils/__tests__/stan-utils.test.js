f376b07fb7338ef59e1f0b94ced1d956eabf1281
fupeng <fupenglxy@gmail.com>
2020-09-21 21:36:44 +0800

    chore: add pkg

---
+++ packages/stan-utils/__tests__/stan-utils.test.js
@@ -0,0 +1,7 @@
+'use strict';
+
+const stanUtils = require('..');
+
+describe('stan-utils', () => {
+    it('needs tests');
+});
---
+++ packages/stan-utils/lib/stan-utils.js
@@ -0,0 +1,7 @@
+'use strict';
+
+module.exports = stanUtils;
+
+function stanUtils() {
+    // TODO
+}
