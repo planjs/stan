912d55c62155b5f52d979e2960356e0b807d8596
fupeng <fupenglxy@gmail.com>
2021-02-05 11:14:29 +0800

    chore: fixtures

---
+++ packages/proto-gen-dts/fixtures/hello.d.ts
@@ -0,0 +1,37 @@
+/** code generate by proto-gen-dts don't edit */
+
+declare namespace hello {
+  export interface HelloService {
+    /** hello */
+    SayName<R extends SayNameReq, O>(r: R, o?: O): Promise<SayNameRsp>;
+  }
+
+  export interface Core {
+    firstName?: string;
+  }
+
+  export const enum Direct {
+    Nil = 0,
+    Up = 1,
+    Down = 2,
+  }
+
+  /** 用户模型 */
+  export interface SayNameReq_User {
+    /** 用户名 */
+    name?: string;
+    avatar?: string;
+  }
+
+  /** 测试评论 */
+  export interface SayNameReq {
+    fullName?: string;
+    core?: Core;
+    user?: SayNameReq_User[];
+    direct?: Direct;
+  }
+
+  export interface SayNameRsp {
+    realName?: string;
+  }
+}
