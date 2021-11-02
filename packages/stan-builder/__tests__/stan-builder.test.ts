3b9e4a2b6dffc5fb2cc30d12e4e1e92e97e10611
fupeng <fupenglxy@gmail.com>
2021-11-03 01:08:18 +0800

    chore: jest

--- packages/stan-builder/__tests__/stan-builder.test.ts
+++ packages/stan-builder/__tests__/stan-builder.test.ts
@@ -3,15 +3,15 @@ import * as fs from 'fs';
 
 import stanLibBuilder from '../src';
 
+jest.useFakeTimers();
+
 const dirs = (p: string, isFullPath = false) =>
   fs
     .readdirSync(p)
     .filter((f) => fs.statSync(path.join(p, f)).isDirectory())
     .map((v) => (isFullPath ? path.join(p, v) : v));
 
-jest.useFakeTimers();
-
-it('stan-builder build', async () => {
+test('tests fixtures', async () => {
   const fixtures = dirs(path.join(__dirname, '../__fixtures__'), true);
   for (const cwd of fixtures) {
     await stanLibBuilder({ cwd });
