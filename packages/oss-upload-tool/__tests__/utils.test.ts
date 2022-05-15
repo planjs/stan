46520e2f4c59e7d0c5de9ede705c4ffafdaa7edd
fupeng <fupenglxy@gmail.com>
2022-05-15 15:35:22 +0800

    chore: testing

--- packages/oss-upload-tool/__tests__/utils.test.ts
+++ packages/oss-upload-tool/__tests__/utils.test.ts
@@ -2,9 +2,11 @@ import { checkOSSFileExits } from '../src/utils';
 
 describe('utils', () => {
   it('checkOSSFileExits', async () => {
-    const res = await checkOSSFileExits('https://www.baidu.com');
+    const res = await checkOSSFileExits(
+      'https://quan-admin-web-1259287960.cos.ap-guangzhou.myqcloud.com/logo.png',
+    );
     expect(typeof res).toBe('string');
-  });
+  }, 5000);
 
   it('checkOSSFileExits ali-oss', async () => {
     try {
