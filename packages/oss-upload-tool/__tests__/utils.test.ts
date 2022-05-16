92ab3436500729644813f3142b2f121aa2588c78
fupeng <fupenglxy@gmail.com>
2022-05-16 10:44:28 +0800

    chore: testing

--- packages/oss-upload-tool/__tests__/utils.test.ts
+++ packages/oss-upload-tool/__tests__/utils.test.ts
@@ -1,16 +1,16 @@
-import { checkOSSFileExits } from '../src/utils';
+import { getRemoteFileInfo } from '../src/utils';
 
 describe('utils', () => {
-  it('checkOSSFileExits', async () => {
-    const res = await checkOSSFileExits(
+  it('getRemoteFileInfo', async () => {
+    const res = await getRemoteFileInfo(
       'https://avatars.githubusercontent.com/u/44866728?s=200&v=4',
     );
     expect(typeof res).toBe('string');
   }, 5000);
 
-  it('checkOSSFileExits ali-oss', async () => {
+  it('getRemoteFileInfo ali-oss', async () => {
     try {
-      const res = await checkOSSFileExits(
+      const res = await getRemoteFileInfo(
         'http://oss-cn-shanghai.aliyuncs.com/__test__/get-upload-list.d.ts',
       );
       expect(typeof res).toBe('string');
@@ -19,9 +19,9 @@ describe('utils', () => {
     }
   });
 
-  it('checkOSSFileExits failed', async () => {
+  it('getRemoteFileInfo failed', async () => {
     try {
-      const res = await checkOSSFileExits('http://xxxx/index.d.ts');
+      const res = await getRemoteFileInfo('http://xxxx/index.d.ts');
       expect(typeof res).toBe('string');
     } catch (e) {
       expect(e).not.toBeDefined();
