b9c7202e39273966470bc44318a2feb3c2b4d3e1
fupeng <fupenglxy@gmail.com>
2022-06-14 22:38:42 +0800

    chore: add testing

--- packages/stan-utils/__tests__/stan-utils.test.ts
+++ packages/stan-utils/__tests__/stan-utils.test.ts
@@ -1,7 +1,23 @@
-import { slash } from '../src';
+import { join } from 'path';
+import { existsSync } from 'fs';
+import { slash, copyFiles } from '../src';
 
 describe('stan-utils', () => {
   it('test slash', () => {
     expect(slash('src\\index.ts')).toBe('src/index.ts');
+  });
+
+  it('test copyFiles', async () => {
+    const dest = 'node_modules/__output__';
+    await copyFiles({
+      targets: {
+        src: 'src/*.ts',
+        dest,
+      },
+      verbose: true,
+      flatten: false,
+      cwd: join(__dirname, '..'),
+    });
+    expect(existsSync(join(__dirname, '../', dest, 'copy-files.ts'))).toBe(true);
   });
 });
